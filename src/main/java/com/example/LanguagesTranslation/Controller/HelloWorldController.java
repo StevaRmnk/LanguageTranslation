a925e91cdecc4586514eeccc56835441559ec815
Stevan Smiljanić <ssmiljanic@comtrade.com>
2023-05-31 09:27:32 +0200

    Added helloworld controller and helloworld rest controller

---
+++ src/main/java/com/example/LanguagesTranslation/Controller/HelloWorldController.java
@@ -0,0 +1,21 @@
+package com.example.LanguagesTranslation.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HelloWorldController {
+
+    @GetMapping(value = "/")
+    public ModelAndView helloWorldDefault(){
+        ModelAndView page = new ModelAndView("helloworld");
+        return  page;
+    }
+
+    @GetMapping(value = "/hello")
+    public ModelAndView helloWorldPage(){
+        ModelAndView page = new ModelAndView("helloworld");
+        return  page;
+    }
+}
--- src/main/java/com/example/LanguagesTranslation/Service/LanguageService.java
+++ src/main/java/com/example/LanguagesTranslation/Service/LanguageService.java
@@ -1,4 +1,15 @@
 package com.example.LanguagesTranslation.Service;
 
+import com.example.LanguagesTranslation.Model.Language;
+
+import java.util.List;
+
 public interface LanguageService {
+
+    List<Language> findAllLanguages();
+    Language findLanguageById(int id);
+
+    Language findLanguageByName(String name);
+
+    Language saveNewLanguage(Language language);
 }
